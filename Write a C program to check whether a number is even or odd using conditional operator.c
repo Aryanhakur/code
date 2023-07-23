d9ce8c995d3d214b57fd0df50ed6a8b79da35121
Aryan Thakur <126138672+Aryanhakur@users.noreply.github.com>
2023-07-23 21:06:04 +0530

    Create Write a C program to check whether a number is even or odd using conditional operator.c

---
+++ Write a C program to check whether a number is even or odd using conditional operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int num;
+int main() {
+   
+    scanf("%d",&num);
+    if(num%2 == 0){
+        printf("given num is even");
+        
+    }
+    else{
+    printf("given num is not even");}
+    return 0;
+}
