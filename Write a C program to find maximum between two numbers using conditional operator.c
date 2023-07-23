d740a4b38e367872517ba8dbf42145d6276a4c8f
Aryan Thakur <126138672+Aryanhakur@users.noreply.github.com>
2023-07-23 21:04:42 +0530

    Create Write a C program to find maximum between two numbers using conditional operator.c

---
+++ Write a C program to find maximum between two numbers using conditional operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int a,b;
+
+int main() {
+    scanf("%d %d  ",&a,&b);
+    if(a>b){
+        
+            printf("a is grater of two number");
+    }
+    else{
+        printf("b is grater of two number");
+    }
+    
+    return 0;
+}
